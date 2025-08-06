a779e175310cce06ec1fe95b7360ffa66dbaafa8
aliry95 <ayazdani@math.ubc.ca>
2025-08-05 20:35:19 -0600

    Remove build dir for tests

--- Tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alireza/Desktop/Work/Codes/GrainsGPU/Tests
-# Build directory: /home/alireza/Desktop/Work/Codes/GrainsGPU/Tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(grains_unit_tests "/home/alireza/Desktop/Work/Codes/GrainsGPU/Tests/build/grains_tests")
-set_tests_properties(grains_unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/alireza/Desktop/Work/Codes/GrainsGPU/Tests/CMakeLists.txt;231;add_test;/home/alireza/Desktop/Work/Codes/GrainsGPU/Tests/CMakeLists.txt;0;")
